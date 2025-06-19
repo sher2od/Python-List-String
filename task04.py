8b63002c51c3ddb2f8d90f5fe5bcbe6bf42abaef
sher2od <sherzodqarshiboyev543@gmail.com>
2025-06-19 15:09:28 +0300

    m4

--- task04.py
+++ task04.py
@@ -1,1 +1,21 @@
+email_enter = input("Emailingiz >> ")
+
+emails = email_enter.split(',')
+
+domains = []
+
+for i in emails:
+    i = i.strip()  # bo'shliqlarni olib tashlash
+    i_index = i.find('@')
+    if i_index != -1:
+        domain = i[i_index:]  # '@' dan boshlab oxirigacha
+        if domain not in domains:  # takrorlanmas bo‘lishi uchun
+            domains.append(domain)
+
+print(domains)
+
+
+
+
+
 
