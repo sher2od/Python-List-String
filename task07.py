8e727c19fc445fe7bc5ab4f0138997b2efdab658
sher2od <sherzodqarshiboyev543@gmail.com>
2025-06-19 19:33:39 +0300

    m7

--- task07.py
+++ task07.py
@@ -1,1 +1,9 @@
 
+malumot = "Ism:Ali, Familiya:Valiyev, Yil:2002"
+
+juftliklar = malumot.split(',')
+
+
+for juftlik in juftliklar:
+    kalit, qiymat = juftlik.split(':')
+    print(f"{kalit.strip()}: {qiymat.strip()}")
