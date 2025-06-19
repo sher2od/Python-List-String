73caaf6a9e0bb515c13b0043f072b1b9056bdc2d
sher2od <sherzodqarshiboyev543@gmail.com>
2025-06-19 14:25:12 +0300

    m1

--- task01.py
+++ task01.py
@@ -1,1 +1,83 @@
 
+
+fish = input("FISH ni kiriting (Familiya Ism Sharif): ")
+
+qisimlar = fish.split()
+
+familiya = qisimlar[0]
+
+ism_sharif = ' '.join(qisimlar[1:])
+
+
+natija = f"{ism_sharif}, {familiya}"
+print(natija)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+''''''
+# nums = [1,2,3,7,8,9]
+# result = []
+
+# i = 0
+# while i < len(nums):
+#     j = i + 1
+#     while j < len(nums):
+#         if nums[i] + nums[j] == 10:
+#             result.append([nums[i],nums[j]])
+#         j += 1
+    
+#     i += 1
+
+# print(result)
+
+''''''
+
+# TODO listlarni bir qatorda yaratish  
+
+
+# nums = [value for value in range(1,10)]
+
+# print(nums)
+
+
+
+# TODO .split()
+
+
+# text = "qizil qora oq kok"
+
+# color = text.split(" ")
+
+# print(color)
+
+
+
+# TODO .join()
+
+# nums = ['65','45','34','3423']
+
+# text = "_".join(nums)
+
+# print(text)
--- task02.py
+++ task02.py
@@ -1,1 +1,13 @@
 
+sohalar = input("Sohalarni vergul bilan ajratib kiriting: ")
+
+sohalar_list = sohalar.split(',')
+
+snake_case_list = [soha.strip().lower().replace(' ', '_') for soha in sohalar_list]
+
+natija = '_'.join(snake_case_list)
+
+print(natija)
+
+
+
